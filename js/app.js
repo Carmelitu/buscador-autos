82f2a6d7b012aaa80bb37ade1bac7054677f4f89
Lucas Seijo <lucaseijo7@gmail.com>
2021-07-20 10:41:34 -0300

    mostrar base de autos

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// Variables
+const resultado = document.querySelector('#resultado');
+
+
+
+// Eventos
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos();
+})
+
+// Funciones
+
+function mostrarAutos(){
+    autos.forEach( auto => {
+        const {marca, modelo, year, precio, puertas, transmision, color} = auto;
+        const autoHTML = document.createElement('p');
+
+        autoHTML.textContent = `
+        ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Precio: $ ${precio} - Color: ${color}
+        `;
+
+        resultado.appendChild(autoHTML);
+    })
+}
+
+
+
