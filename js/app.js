a795c1c982dfccfcdb45b3c81f8cee4a9e2f2835
Lucas Seijo <lucaseijo7@gmail.com>
2021-07-21 11:36:41 -0300

    carga de objeto segun seleccion

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
 // Variables
-const resultado = document.querySelector('#resultado');
 const year = document.querySelector('#year');
+const marca = document.querySelector('#marca');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor resultados
+const resultado = document.querySelector('#resultado');
+
 const maxYear = new Date().getFullYear();
 const minYear = maxYear - 11;
+
+
+// Generar objeto con la busqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+}
+
 
 // Eventos
 
@@ -11,6 +33,30 @@ document.addEventListener('DOMContentLoaded', () => {
     
 
     llenarSelect();
+})
+
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+})
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+})
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+})
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+})
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+})
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
 })
 
 // Funciones
